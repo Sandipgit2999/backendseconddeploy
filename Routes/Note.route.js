ebbb990173468a86fd5413491feab9cc4c95ca03
Sandip <suryawanshisandip2999@gmail.com>
2022-11-06 17:51:07 +0530

    updated notes route post and put method

--- Routes/Note.route.js
+++ Routes/Note.route.js
@@ -22,39 +22,104 @@ Notecontroller.post("/create", async (req, res) => {
   const payload = req.body;
   console.log(payload);
 
-  const file = req.files.photo;
-  const videofile = req.files.video;
+  //console.log("jfkdjfksjfksjfskfjskfj", req.files);
+  let file;
+  if (req.files) {
+    file = req.files.photo;
+  }
+  //console.log("flie-------", file);
+  //const videofile = req.files.video;
   //console.log(file,videofile)
 
+  if (file) {
+    cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
+      console.log(result);
+      if (result) {
+        if (payload.title && payload.note && payload.tag) {
+          const new_note = new NoteModel(payload);
 
-  
-  cloudinary.uploader.upload(videofile.tempFilePath, async (err, result) => {
-    console.log(result);
-    // if (payload.title && payload.note && payload.tag) {
-     
-    //   const new_note = new NoteModel(payload);
-      
-    //   new_note.videourl = result.url;
-    //   await new_note.save();
-    //   res.send("successfully added");
-    // } else {
-    //   res.send("plese fill all details");
-    // }
-    res.send("successfully  ")
-  });
-  
+          new_note.imageurl = result.url;
+          await new_note.save();
+          res.send("successfully added");
+        } else {
+          res.send("plese fill all details");
+        }
+      } else if (err) {
+        res.send({ msg: "something went wrong in file updloading" });
+      } else {
+        if (payload.title && payload.note && payload.tag) {
+          const new_note = new NoteModel(payload);
+          await new_note.save();
+          res.send("successfully added");
+        } else {
+          res.send("plese fill all details");
+        }
+      }
+    });
+  } else {
+    if (payload.title && payload.note && payload.tag) {
+      const new_note = new NoteModel(payload);
+      await new_note.save();
+      res.send("successfully added");
+    } else {
+      res.send("plese fill all details");
+    }
+  }
 });
 
 Notecontroller.put("/:id", async (req, res) => {
   const payload = req.body;
   const { id } = req.params;
-  const newNote = await NoteModel.updateOne(
-    { _id: id, userId: req.body.userId },
-    {
-      $set: payload,
-    }
-  );
-  res.send("successfull update");
+  console.log(typeof id);
+
+  // const newNote = await NoteModel.findOne({
+  //   _id: `"${id}"`,
+  //   userId: req.body.userId,
+  // });
+
+  //console.log("------------------nwe main node",newNote)
+
+  let file;
+  //console.log("req.files", req.files);
+  if (req.files) {
+    file = req.files.photo;
+  }
+
+  //console.log("file", file);
+
+  if (file) {
+    cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
+      //console.log("reult-url", result);
+      if (result) {
+        const newNote = await NoteModel.updateOne(
+          { _id: id, userId: req.body.userId },
+          {
+            $set: { ...payload, imageurl: result.url },
+          }
+        );
+        console.log("newNote", newNote);
+        res.send({ msg: "successfullly updated note" });
+      } else if (err) {
+        res.send({ msg: "something went wrong in file updloading" });
+      } else {
+        const newNote = await NoteModel.updateOne(
+          { _id: id, userId: req.body.userId },
+          {
+            $set: payload,
+          }
+        );
+        res.send({ msg: "successfullly updated note" });
+      }
+    });
+  } else {
+    const newNote = await NoteModel.updateOne(
+      { _id: id, userId: req.body.userId },
+      {
+        $set: payload,
+      }
+    );
+    res.send("successfull updated");
+  }
 });
 
 Notecontroller.delete("/:id", async (req, res) => {
